0cc317ce166a18d806431aed32c7b231c5cf7cd0
iYigitOnay <mavi221907@gmail.com>
2025-07-19 13:48:39 +0300

    📂 Çok Katmanlı Route Sistemi Oluşturuldu
    Ana rota ("/") ile birlikte "/blog" ve "/blog/1128" için özel yanıtlar tanımlandı. Her route için erişim loglaması eklendi. Ayrıca middleware zincirinde `next()` fonksiyonu kullanılmaya başlandı.
    
    Defined specific HTML responses for "/", "/blog", and "/blog/1128" routes. Added access logging via console.log for each route. Introduced use of `next()` to enable middleware chaining.

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/blog", function (req, res) {
-  console.log("About route accessed");
-  res.send("<html><body><h1>About Page</h1></body></html>");
-});
-
-app.use("/blog/1128", function (req, res) {
+app.use("/blog/1128", function (req, res, next) {
   console.log("Blog post 1128 accessed");
   res.send("<html><body><h1>Blog Post 1128</h1></body></html>");
+  next();
 });
+
+app.use("/blog", function (req, res, next) {
+  console.log("About route accessed");
+  res.send("<html><body><h1>Blog Page</h1></body></html>");
+  next();
+});
+
+app.use("/", function (req, res) {
+  console.log("Home route accessed");
+  res.send("<html><body><h1>Home Page</h1></body></html>");
+});
+
+// Start the server
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
