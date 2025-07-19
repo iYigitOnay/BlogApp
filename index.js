3b545ff44f08409fb5bbcaaf8945f684893e3449
iYigitOnay <mavi221907@gmail.com>
2025-07-19 13:45:08 +0300

    🌐 Yeni Blog Rotaları Eklendi
    'Home Page' endpoint'i kaldırıldı. Yerine /blog ve /blog/1128 için iki ayrı route tanımlandı. HTML yanıtlar ve debug amaçlı console.log çıktıları eklendi.
    
    Removed the general 'Home Page' endpoint. Added two specific routes for /blog and /blog/1128. HTML-based responses and debug-friendly console.log messages were implemented.

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use(function (req, res) {
-  res.end("Home Page!");
+app.use("/blog", function (req, res) {
+  console.log("About route accessed");
+  res.send("<html><body><h1>About Page</h1></body></html>");
+});
+
+app.use("/blog/1128", function (req, res) {
+  console.log("Blog post 1128 accessed");
+  res.send("<html><body><h1>Blog Post 1128</h1></body></html>");
 });
 
 app.listen(3000, () => {
