34e10b43e7c986a5688401589e90ad4a62296590
iYigitOnay <mavi221907@gmail.com>
2025-07-20 23:41:48 +0300

    🧩 Statik Dosya Yönlendirme ve Bootstrap Tabanlı Sayfa Tasarımları Tamamlandı
    Projenin dosya yapısı yeniden düzenlendi. /libs ve /static path'leri ile dış kütüphaneler ve görseller erişilebilir hale getirildi. index.html ve blogs.html dosyaları Bootstrap ile yeniden tasarlandı. Kartlar, navbar ve sidebar bileşenleri eklendi.
    
    Restructured the project for modular static file access using /libs and /static paths. Redesigned index.html and blogs.html with Bootstrap. Added cards, navigation bar, and sidebar elements to enhance layout and user experience.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static("node_modules"));
+app.use("/libs", express.static(path.join(__dirname, "node_modules")));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use("/blogs/:blogid", function (req, res) {
   res.sendFile(path.join(__dirname, "views", "users", "blog-details.html"));
